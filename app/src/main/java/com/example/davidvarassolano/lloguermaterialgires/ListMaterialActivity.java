d5ce673a0d1c87aa0e5597fbd9a85cdeefe208bf
david.varas.solano <dvarasolano@gmail.com>
2019-01-04 21:13:34 +0100

    Amb aquesta versió, quan cliquem sobre al botó de la lupa en NormalUser Activity, veiem tot el catàleg de material disponible. Només el nom

--- app/src/main/java/com/example/davidvarassolano/lloguermaterialgires/ListMaterialActivity.java
+++ app/src/main/java/com/example/davidvarassolano/lloguermaterialgires/ListMaterialActivity.java
@@ -3,9 +3,16 @@ package com.example.davidvarassolano.lloguermaterialgires;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -14,6 +21,7 @@ public class ListMaterialActivity extends AppCompatActivity {
     private ArrayAdapter adapter;
     private ArrayList<String> list_item;
     private ListView list_material;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +31,25 @@ public class ListMaterialActivity extends AppCompatActivity {
         list_material = findViewById(R.id.list_material);
         Intent intent = getIntent();
         list_item = new ArrayList<>();
-        list_item.add("Mosquetons");
-        list_item.add("Dissipador");
-        list_item.add("Corda 50m");
-        list_item.add("Casc escalada");
-        list_item.add("Neopreno");
+        db.collection("Catalogo").addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if (e != null){
+                    Log.e("LloguerMaterialGires","Firestore Error: "+e.toString());
+                    return;
+                }
+                list_item.clear();
+                for (DocumentSnapshot doc: documentSnapshots){
+                    list_item.add(doc.getString("nombre"));
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
+        //list_item.add("Mosquetons");
+        //list_item.add("Dissipador");
+        //list_item.add("Corda 50m");
+        //list_item.add("Casc escalada");
+        //list_item.add("Neopreno");
         adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list_item);
         list_material.setAdapter(adapter);
     }
