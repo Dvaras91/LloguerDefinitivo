dd61edca5a4912c013ffc5ce3b4a17ae799ea521
david.varas.solano <dvarasolano@gmail.com>
2019-01-07 19:51:25 +0100

    Els usuaris ja poden entrar amb el seu id y contrasenya. Acabar de parlar algunes coses amb el pau.

--- app/src/main/java/com/example/davidvarassolano/lloguermaterialgires/AccesUserActivity.java
+++ app/src/main/java/com/example/davidvarassolano/lloguermaterialgires/AccesUserActivity.java
@@ -1,13 +1,65 @@
 package com.example.davidvarassolano.lloguermaterialgires;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class AccesUserActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    EditText Usuari, Pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acces_user);
+        Usuari = findViewById(R.id.txt_usuari);
+        Pass = findViewById(R.id.txt_psw);
+
+
+    }
+
+
+    public void entryApp(View view) {
+        final String usuari = Usuari.getText().toString();
+        final String pass = Pass.getText().toString();
+        db.collection("Usuarios").whereEqualTo("id_usuario",usuari).whereEqualTo("pass",pass).addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if (e!=null){
+                    Log.e("LloguerMaterialGires","Firestore Error:"+e.toString());
+                    ErrorUser();
+                    return;
+                }
+                for (DocumentSnapshot doc: documentSnapshots){
+                    if (usuari.equals(doc.getString("id_usuario")) && (pass.equals(doc.getString("pass")))){
+                        CorrectUser();
+
+                    }
+                    else {
+                        ErrorUser();
+                    }
+                }
+
+            }
+        });
+    }
+
+    void ErrorUser(){
+        Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show();
+    }
+    void CorrectUser(){
+        Intent intent = new Intent(this,NormalUserActivity.class);
+        startActivity(intent);
     }
 }
